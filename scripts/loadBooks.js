4d793b556c3ca772d46f0e7c898152857ae778f4
JdM <julien@rmbi.ch>
2023-08-27 19:51:34 +0200

    Show cover image

--- scripts/loadBooks.js
+++ scripts/loadBooks.js
@@ -6,18 +6,20 @@ async function getBooks() {
 
 function displayBooks(books, booksEmp) {
     let booksHtml = "";
+
     books.forEach(book => {
-        booksHtml += getBookHtml(book.title, book.code, book.ID);
+        booksHtml += getBookHtml(book.title, book.code, book.ID, book.coverUrl);
     });
 
     booksEmp.innerHTML = booksHtml;
 }   
 
-function getBookHtml(title, code, id){
+function getBookHtml(title, code, id, coverUrl){
     return `
         <div id='book_${id}'>
             <h2>${title}</h2>
             <h3>${code}</h3>
+            <img src="${coverUrl}" alt="${title} Cover">
         </div>
     `;
 }
